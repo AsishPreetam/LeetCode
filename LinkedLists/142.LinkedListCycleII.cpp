196873f5fe9b3e3a22c6dac52877043a9ceefc25
Asish Preetam Garikina <asishpreetam.garikina@gmail.com>
2026-07-05 19:25:50 +0530

    Add cycle detection method for linked lists
    Implement cycle detection in a linked list using a hash map.

---
+++ LinkedLists/142.LinkedListCycleII.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        unordered_map<ListNode*, int> mp;
+        ListNode* temp = head;
+        while(temp!=nullptr){
+            if(mp.find(temp)==mp.end()){
+                mp[temp] = 1;
+            }
+            else{
+                return temp;
+            }
+            temp = temp->next; 
+        }
+        return nullptr;
+    }
+};
+/*
+Time complexity : O(n)
+Space complexity : O(n)
+*/
