d6d6e87d520a920c1dcb4944be2ee4b96583f54c
Asish Preetam Garikina <asishpreetam.garikina@gmail.com>
2026-07-05 19:00:37 +0530

    Update 003_LongestSubstringWithoutRepeatingCharacters.cpp

--- Arrays/003_LongestSubstringWithoutRepeatingCharacters.cpp
+++ Arrays/003_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        unordered_map<char, int> freq;
-        int max_len = 0;
-        int i=0, j=0;
-        while(j<n && i<n){
-            if(freq.find(s[j]) == freq.end()){
-                freq[s[j]]++;
-                max_len = max(max_len, j-i+1);
-                j++;
+        unordered_map<char, int> lastIndex;
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            if (lastIndex.count(s[right]) &&
+                lastIndex[s[right]] >= left) {
+                left = lastIndex[s[right]] + 1;
             }
-            else if(freq.find(s[j]) != freq.end()){
-                freq.clear();
-                i++;
-                j=i;
-            }  
+
+            lastIndex[s[right]] = right;
+            ans = max(ans, right - left + 1);
         }
-        return max_len;
+
+        return ans;
     }
 };
