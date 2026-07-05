c2d5925b002d5415a328c5ae99789609b930934b
Asish Preetam Garikina <asishpreetam.garikina@gmail.com>
2026-07-05 19:06:31 +0530

    Create 1021_RemoveOutermostParanthesis.cpp

---
+++ Strings/1021_RemoveOutermostParanthesis.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        stack<char> st;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='('){
+                if(!st.empty()){
+                    ans.push_back(s[i]);
+                }
+                st.push(s[i]);
+            }
+            else{
+                st.pop();
+                if(!st.empty()){
+                    ans.push_back(s[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
