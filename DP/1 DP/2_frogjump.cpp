d6b3a195a7b6d7ce836f0ecb3975c9db1829ad72
Asish Preetam Garikina <asishpreetam.garikina@gmail.com>
2026-09-03 21:02:02 +0530

    Implement frog jump solution in DP/2_frogjump.cpp

---
+++ DP/1 DP/2_frogjump.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int frogJump(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> dp(n);
+        dp[0] = 0;
+        dp[1] = dp[0] + abs(heights[1] - heights[0]);
+        for(int i = 2; i<n; i++){
+            dp[i] = min(dp[i-1] + abs(heights[i] - heights[i-1]),
+                        dp[i-2] + abs(heights[i] - heights[i-2]));
+        }
+        return dp[n-1];
+    }
+};
